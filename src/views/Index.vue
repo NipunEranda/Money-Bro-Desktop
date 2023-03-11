deaf806385c554ebb894ac4e0ae90f314b066df6
NipunEranda <namarasekara71@gmail.com>
2023-03-11 21:51:54 +0530

    file handling added

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,27 +1,61 @@
 <template>
     <div class="main">
-        <img class="main-logo center" src="../assets/310x310.png" />
+        <img class="main-logo center" src="../assets/icon.png" />
         <h1 class="main-title">Welcome To MoneyBro</h1>
-        <button class="btn btn-sm btn-primary mt-3">Continue</button>
+        <button class="btn btn-sm btn-primary mt-3" @click="goToInit()">Get Started</button>
     </div>
-    <!-- <button class="btn btn-sm btn-light" @click="$router.push('/about')">Go to About</button> -->
 </template>
 
 <script>
 
+import * as fs from '@tauri-apps/api/fs';
+import * as tauriPath from '@tauri-apps/api/path';
+import * as shell from '@tauri-apps/api/shell';
+import { invoke } from '@tauri-apps/api/tauri';
+import { notify } from '../utils';
+
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        goToInit: async function () {
+            this.$router.push('/init');
+            await this.createFile();
+        },
+        createFile: async function () {
+            // https://tauri.app/v1/api/js/modules/fs
+            const appObj = {
+                createdOn: (new Date().toLocaleString())
+            };
+
+            const filePath = `${await tauriPath.documentDir()}MoneyBro\\config.json`;
+            fs.readTextFile(filePath).then((data) => {
+                console.log('file exists');
+            }).catch(async e => {
+                if (e.includes('os error 3')) {
+                    await fs.createDir('MoneyBro', { dir: fs.BaseDirectory.Document, recursive: true })
+                    await fs.writeTextFile('MoneyBro\\config.json', JSON.stringify(appObj, 2, null), { dir: fs.BaseDirectory.Document });
+                    notify('User Data', 'User data store is located in Documents/MoneyBro');
+                } else {
+                    notify('Something went wrong', e);
+                }
+            })
+
+            console.log(await fs.readTextFile(filePath));
+        }
+    }
+};
+
+
 </script>
 
 <style scoped>
-.main {
-    margin-top: 20%;
-    display: grid;  
-}
-
-.main-logo{
+.main-logo {
     width: 150px;
 }
 
-.btn{
+.btn {
     margin: auto;
     padding: 5px 20px 5px 20px;
 }
---
+++ src/views/Initial-Setup/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="main init">
+      <div class="form">
+        Init
+      </div>
+      <div class="buttons">
+        <button class="btn btn-sm btn-primary float-right ms-2">Next</button>
+        <button class="btn btn-sm btn-secondary float-right"  @click="this.$router.push('/')">Previous</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.init{
+  background-color: #414141;
+  padding: 10px;
+}
+
+.buttons{
+  align-content: right;
+}
+
+</style>
